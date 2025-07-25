81e7752f4522df6eda0df072426c6418032b6015
Balachander <166808771+balachander025@users.noreply.github.com>
2025-07-25 17:49:56 +0530

    Create MainApplication.java

---
+++ MainApplication.java
@@ -0,0 +1,13 @@
+package com.bala.micoservice_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicoserviceProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicoserviceProductApplication.class, args);
+	}
+
+}
